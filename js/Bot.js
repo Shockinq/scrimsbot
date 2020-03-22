3ed1bcfc9761e5459c18950cc4e0148ceaff6504
Shockinq <53934940+Shockinq@users.noreply.github.com>
2020-03-22 12:45:21 +0800

    changed important files

--- js/Bot.js
+++ js/Bot.js
@@ -66,7 +66,6 @@ Bot.initEvents = function() {
 
 Bot.login = function() {
 	this.bot.login(this.DBM.Files.data.settings.token);
-	this.bot.login(process.env.token);
 };
 
 Bot.onReady = function() {
